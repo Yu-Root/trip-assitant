357e59cb2a2a91dd194d656ab64713b0a1721970
Homfay-0903 <2790165595@qq.com>
2025-12-01 16:53:42 +0800

    Ver-0.4687

--- src/views/User/components/dialog/index.vue
+++ src/views/User/components/dialog/index.vue
@@ -1,12 +1,29 @@
 <template>
     <el-dialog v-model="dialogSettingVisible" title="修改" align-center>
-        123
+        <el-form :model="nameForm">
+            <el-form-item>
+                <template v-slot:label>
+                    <span>旧名称</span>
+                </template>
+                <el-input v-model="nameForm.oldName"></el-input>
+            </el-form-item>
+            <el-form-item>
+                <template v-slot:label>
+                    <span>新名称</span>
+                </template>
+                <el-input v-model="nameForm.newName" placeholder="输入你的新名字"></el-input>
+            </el-form-item>
+        </el-form>
     </el-dialog>
 </template>
 
 <script setup>
-import { ref } from 'vue';
+import { reactive, ref } from 'vue';
 
+const nameForm = reactive({
+    oldName: '',
+    newName: ''
+})
 
 const dialogSettingVisible = ref(false)
 const openDialog = () => {
