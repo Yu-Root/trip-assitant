bffd1f062115bc88fb26703c3ee40e54a3ca9e87
Homfay-0903 <2790165595@qq.com>
2025-12-02 19:03:32 +0800

    Ver-0.4695

--- src/views/User/components/dialog/index.vue
+++ src/views/User/components/dialog/index.vue
@@ -1,16 +1,21 @@
 <template>
     <el-dialog v-model="dialogSettingVisible" :title="tilte" align-center @close="handleCancel">
-        <el-form :model="nameForm">
-            <el-form-item>
-                <el-input v-model="nameForm.oldName"></el-input>
-            </el-form-item>
-            <el-form-item>
-                <template v-slot:label>
-                    <span>新名称</span>
-                </template>
-                <el-input v-model="nameForm.newName" placeholder="输入你的新名字"></el-input>
+        <el-form :model="formData" :rules="rules" ref="formRef">
+            <el-form-item v-for="field in fields" :key="field.key" :label="field.label" :prop="field.key">
+                <el-input v-if="field.type === 'input'" v-model="formData[field.key]"
+                    :placeholder="field.placeholder || ''"></el-input>
+
+                <el-select v-if="field.type === 'select'" v-model="formData[field.key]"
+                    :placeholder="field.placeholder || ''">
+                    <el-option v-for="opt in field.options" :key="opt.value" :label="opt.label"
+                        :value="opt.value"></el-option>
+                </el-select>
             </el-form-item>
         </el-form>
+        <template #footer>
+            <el-button @click="handleCancel">取消</el-button>
+            <el-button type="primary" @click="handleConfirm">确认</el-button>
+        </template>
     </el-dialog>
 </template>
 
@@ -63,11 +68,25 @@ watch(
 const emit = defineEmits(['confirm', 'cancel', 'update:modelValue'])
 
 const handleConfirm = () => {
+    formRef.value.validate((valid) => {
+        if (valid) {
+            emit('confirm', { ...formData })
+            dialogSettingVisible.value = false
+            emit('update:modelValue', false)
+        }
+    })
+}
 
+const handleCancel = () => {
+    emit('cancel')
+    dialogSettingVisible.value = false
+    emit('update:modelValue', false)
+    formRef.value.resetFields()
 }
 
 const openDialog = () => {
     dialogSettingVisible.value = true
+    emit('update:modelValue', true)
 }
 defineExpose({
     openDialog
