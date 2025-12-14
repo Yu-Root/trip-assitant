5d9fd14ecb42304ed4bc033674ea9a730fd6f3e1
Homfay-0903 <2790165595@qq.com>
2025-12-14 13:33:51 +0800

    Ver-0.4685

---
+++ src/views/TravelPlan/components/cityInfo.vue
@@ -0,0 +1,13 @@
+<template>
+    <el-dialog v-model="dialogCityInfoVisible" :title="title" align-center @close="handleCancel">
+
+    </el-dialog>
+</template>
+
+<script setup>
+import { reactive, ref, watch } from 'vue';
+
+
+</script>
+
+<style lang="scss" scoped></style>
