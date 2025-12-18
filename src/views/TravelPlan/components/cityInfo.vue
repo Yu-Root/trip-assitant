d839dfa33a651574d25e82ffe81c469efe69f0e0
Homfay-0903 <2790165595@qq.com>
2025-12-18 20:19:06 +0800

    Ver-0.4687

--- src/views/TravelPlan/components/cityInfo.vue
+++ src/views/TravelPlan/components/cityInfo.vue
@@ -7,7 +7,37 @@
 <script setup>
 import { reactive, ref, watch } from 'vue';
 
+const props = defineProps({
+    title: {
+        title: String,
+        require: true
+    },
+    dialogValue: {
+        type: Boolean,
+        default: false
+    }
+})
 
+const dialogCityInfoVisible = ref(props.dialogValue)
+
+watch(
+    () => props.dialogValue,
+    (newVal) => {
+        dialogCityInfoVisible.value = newVal
+    }
+)
+
+const emit = defineEmits(['confirm', 'cancel'])
+
+const handleConfirm = () => {
+    emit('confirm')
+    dialogCityInfoVisible.value = false
+}
+
+const handleCancel = () => {
+    emit('cancel')
+    dialogCityInfoVisible.value = false
+}
 </script>
 
 <style lang="scss" scoped></style>
